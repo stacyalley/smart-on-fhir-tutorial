02830e10ed5f9ee11a96bbdb29bf160ad691e7c1
stacyalley <31012756+stacyalley@users.noreply.github.com>
2017-08-14 11:14:53 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -51,7 +51,7 @@
           var p = defaultPatient();
           p.birthdate = dobStr;
           p.gender = gender;
-          p.maritalStatus = maritalStatus;
+          p.maritalStatus = 'hello';//maritalStatus;
           p.fname = fname;
           p.lname = lname;
           p.age = parseInt(calculateAge(dob));
